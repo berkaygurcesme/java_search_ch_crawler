b4d56b9da64d3d5b683dc87530ff42c07646f868
berkaygurcesme <93475219+berkaygurcesme@users.noreply.github.com>
2022-09-14 16:07:43 +0300

    first commit

---
+++ src/main/java/com/javacrawler/searchch/SearchchApplication.java
@@ -0,0 +1,13 @@
+package com.javacrawler.searchch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacrawler/searchch/SearchchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacrawler.searchch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
